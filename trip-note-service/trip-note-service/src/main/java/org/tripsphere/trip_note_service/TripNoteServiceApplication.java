6b3a84d7e046567cd912ac08bd295cb1ea6905bb
WangRongze <23302010072@m.fudan.edu.cn>
2025-10-01 00:10:51 +0800

    chore: initialize note servie SpringBoot project framework

---
+++ trip-note-service/trip-note-service/src/main/java/org/tripsphere/trip_note_service/TripNoteServiceApplication.java
@@ -0,0 +1,13 @@
+package org.tripsphere.trip_note_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripNoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripNoteServiceApplication.class, args);
+	}
+
+}
---
+++ trip-note-service/trip-note-service/src/test/java/org/tripsphere/trip_note_service/TripNoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tripsphere.trip_note_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripNoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
