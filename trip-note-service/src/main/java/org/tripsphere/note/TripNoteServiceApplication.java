66f7ff817e0b103fc940f1c2a5c50e7bf477dd07
Senyu Xie <senyuxie@qq.com>
2025-10-02 18:43:40 +0800

    feat: Implement GetVersion of MetadataService in trip-note-service.

--- trip-note-service/src/main/java/org/tripsphere/note/TripNoteServiceApplication.java
+++ trip-note-service/src/main/java/org/tripsphere/note/TripNoteServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TripNoteServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TripNoteServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TripNoteServiceApplication.class, args);
+    }
 }
