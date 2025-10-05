8148c3059797dda30f516de88f31ce4e2d48b78f
Senyu Xie <senyuxie@qq.com>
2025-10-05 22:25:00 +0800

    refactor: Rename SpringBootApplication of trip-note-service.

--- trip-note-service/src/main/java/org/tripsphere/note/NoteApplication.java
+++ trip-note-service/src/main/java/org/tripsphere/note/NoteApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TripNoteServiceApplication {
+public class NoteApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TripNoteServiceApplication.class, args);
+        SpringApplication.run(NoteApplication.class, args);
     }
 }
