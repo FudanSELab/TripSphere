2574bb628a197cf868587f024bcfa6d5dc1c0425
Senyu Xie <senyuxie@qq.com>
2025-10-13 19:01:53 +0800

    feat: Update dependencies and implement gRPC MetadataService for version retrieval.

---
+++ trip-note-service/src/main/java/org/tripsphere/note/grpc/MetadataServiceImpl.java
@@ -0,0 +1,32 @@
+package org.tripsphere.note.grpc;
+
+import io.grpc.stub.StreamObserver;
+
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.tripsphere.note.GetVersionRequest;
+import org.tripsphere.note.GetVersionResponse;
+import org.tripsphere.note.MetadataServiceGrpc;
+
+@GrpcService
+public class MetadataServiceImpl extends MetadataServiceGrpc.MetadataServiceImplBase {
+    private final BuildProperties buildProperties;
+
+    public MetadataServiceImpl(@Autowired(required = false) BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
+    @Override
+    public void getVersion(
+            GetVersionRequest request, StreamObserver<GetVersionResponse> responseObserver) {
+        String version =
+                buildProperties != null
+                        ? buildProperties.getVersion()
+                        : "develop"; // Default version in development environment
+        GetVersionResponse response = GetVersionResponse.newBuilder().setVersion(version).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
