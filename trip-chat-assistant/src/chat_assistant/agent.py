f228f2a6afa844c10a25f8d59095a81563796593
Senyu Xie <senyuxie@qq.com>
2025-12-07 18:22:28 +0800

    feat: Add mcp-weather-server for chat_assistant and update tool config.

--- trip-chat-assistant/src/chat_assistant/agent.py
+++ trip-chat-assistant/src/chat_assistant/agent.py
@@ -1,40 +1,49 @@
 import logging
+import warnings
 from datetime import datetime
 
 from dotenv import load_dotenv
-from google.adk.agents import LlmAgent
 from google.adk.a2a.utils.agent_to_a2a import to_a2a
+from google.adk.agents import LlmAgent
+from google.adk.agents.readonly_context import ReadonlyContext
 from google.adk.models.lite_llm import LiteLlm
+from google.adk.tools.mcp_tool import McpToolset
+from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams
+from mcp import StdioServerParameters
 
-logger = logging.getLogger(__name__)
+warnings.filterwarnings("ignore")  # Suppress ADK Experimental Warnings
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
 
-def fetch_weather(location: str, timestamp: datetime | None = None) -> str:
-    """
-    Fetches weather information for a given location at a specific timestamp.
-
-    Arguments:
-        location: The location to get the weather for.
-        timestamp: Optional datetime for which to get the weather.
-            Defaults to the current datetime.
-
-    Returns:
-        A string describing the weather at the location and time.
-    """
-    # Placeholder implementation
-    if timestamp is None:
-        timestamp = datetime.now()
-    print(f"Fetching weather for {location} at {timestamp}")
-    return f" {location} is sunny with a temperature of 25℃ at {timestamp}."
+weather_toolset = McpToolset(
+    connection_params=StdioConnectionParams(
+        server_params=StdioServerParameters(
+            command="python", args=["-m", "mcp_weather_server"]
+        )
+    )
+)
 
 
-agent = LlmAgent(
+INSTRUCTION = """
+Role: You are a helpful agent integrated with weather information tools.
+
+Current Datetime (with Timezone): {current_datetime}
+""".strip()
+
+
+def root_instruction(ctx: ReadonlyContext) -> str:
+    # Get current datetime with timezone
+    current_datetime = datetime.now().astimezone().isoformat()
+    return INSTRUCTION.format(current_datetime=current_datetime)
+
+
+root_agent = LlmAgent(
     model=LiteLlm(model="openai/gpt-4o-mini"),
     name="chat_assistant",
-    description="An agent that can help with weather information.",
-    instruction="Use the fetch_weather tool to get weather information.",
-    tools=[fetch_weather],
+    description="An agent that can help users with weather information.",
+    instruction=root_instruction,
+    tools=[weather_toolset],
 )
-app = to_a2a(agent)
+app = to_a2a(root_agent)
