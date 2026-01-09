570dedb47ed672c2b95126a8ed4d9cdced88e644
LJCARG <1604137514@qq.com>
2026-01-09 15:28:49 +0800

    refector: Use ruff check.

--- trip-review-summary/src/review_summary/grpc/attraction.py
+++ trip-review-summary/src/review_summary/grpc/attraction.py
@@ -1,9 +1,14 @@
 import grpc
-from tripsphere.attraction.attraction_pb2 import FindIdByNameRequest, FindIdByNameResponse
+from tripsphere.attraction.attraction_pb2 import (
+    FindIdByNameRequest,
+    FindIdByNameResponse,
+)
 from tripsphere.attraction.attraction_pb2_grpc import AttractionServiceStub
-from review_summary.config.settings import (get_settings)
+
+from review_summary.config.settings import get_settings
 
 settings = get_settings()
+
 
 def find_attraction_id_by_name(name: str) -> str:
     host = settings.attraction_service.host
