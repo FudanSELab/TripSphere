c7d3be9a8d3d96daad18d61111408146ae847404
SenyuXie <senyuxie@qq.com>
2026-01-17 16:30:52 +0800

    feat: Update User model to use String for ID and implement UUID generation on persist.

--- trip-user-service/src/main/java/org/tripsphere/user/model/UserEntity.java
+++ trip-user-service/src/main/java/org/tripsphere/user/model/UserEntity.java
@@ -1,5 +1,7 @@
 package org.tripsphere.user.model;
 
+import com.github.f4b6a3.uuid.UuidCreator;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,8 +14,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(length = 36, nullable = false)
+    private String id;
 
     @Column(unique = true, nullable = false)
     private String username;
@@ -26,4 +28,11 @@ public class UserEntity {
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "role")
     private java.util.Set<Role> roles = new java.util.HashSet<>();
+
+    @PrePersist
+    public void generateId() {
+        if (this.id == null) {
+            this.id = UuidCreator.getTimeOrderedEpoch().toString();
+        }
+    }
 }
--- trip-user-service/src/main/java/org/tripsphere/user/repository/UserRepository.java
+++ trip-user-service/src/main/java/org/tripsphere/user/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import org.tripsphere.user.model.UserEntity;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, String> {
     Optional<UserEntity> findByUsername(String username);
 
     boolean existsByUsername(String username);
