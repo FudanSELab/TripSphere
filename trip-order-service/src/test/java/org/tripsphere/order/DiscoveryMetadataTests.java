15ef6b643b53e24f480783e858a4cd5a11c0bcf1
Dqchen77 <2091984032@qq.com>
2026-08-24 20:17:20 +0800

    fix(discovery): add grpc metadata to product inventory order

---
+++ trip-order-service/src/test/java/org/tripsphere/order/DiscoveryMetadataTests.java
@@ -0,0 +1,26 @@
+package org.tripsphere.order;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+@SpringBootTest(properties = {
+    "spring.cloud.nacos.discovery.enabled=false",
+    "spring.cloud.nacos.discovery.register-enabled=false"
+})
+class DiscoveryMetadataTests {
+
+    @Autowired
+    private Environment environment;
+
+    @Test
+    void exposesGrpcDiscoveryMetadata() {
+        assertThat(environment.getProperty("spring.cloud.nacos.discovery.metadata.gRPC_port"))
+            .isEqualTo(environment.getProperty("grpc.server.port"));
+        assertThat(environment.getProperty("spring.cloud.nacos.discovery.metadata.protocol"))
+            .isEqualTo("grpc");
+    }
+}
